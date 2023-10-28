675497b16b9071efc3934ea3e8287eaa5b63d413
moisamidi <moisamidi.com>
2023-10-28 07:34:42 +0200

    added files

--- src/main/java/ua/edu/ucu/apps/flowerstore/FlowersApplication.java
+++ src/main/java/ua/edu/ucu/apps/flowerstore/FlowersApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FlowerstoreApplication {
+public class FlowersApplication {
 
 	public static void main(String[] args)
 	{
-		SpringApplication.run(FlowerstoreApplication.class, args);
+		SpringApplication.run(FlowersApplication.class, args);
 	}
 
 }
